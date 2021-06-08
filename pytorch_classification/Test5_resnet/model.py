b121048dd8cb320a95b9905ce2d62ba701a49d66
Wangmt098 <81133997+Wangmt098@users.noreply.github.com>
2021-06-08 16:46:53 +0800

    Update model.py

--- pytorch_classification/Test5_resnet/model.py
+++ pytorch_classification/Test5_resnet/model.py
@@ -43,21 +43,18 @@ class Bottleneck(nn.Module):
     """
     expansion = 4
 
-    def __init__(self, in_channel, out_channel, stride=1, downsample=None,
-                 groups=1, width_per_group=64):
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
 
-        width = int(out_channel * (width_per_group / 64.)) * groups
-
-        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width,
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
                                kernel_size=1, stride=1, bias=False)  # squeeze channels
-        self.bn1 = nn.BatchNorm2d(width)
+        self.bn1 = nn.BatchNorm2d(out_channel)
         # -----------------------------------------
-        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups,
-                               kernel_size=3, stride=stride, bias=False, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
+                               kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(width)
         # -----------------------------------------
-        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel*self.expansion,
+        self.conv3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel*self.expansion,
                                kernel_size=1, stride=1, bias=False)  # unsqueeze channels
         self.bn3 = nn.BatchNorm2d(out_channel*self.expansion)
         self.relu = nn.ReLU(inplace=True)
@@ -91,9 +88,7 @@ class ResNet(nn.Module):
                  block,
                  blocks_num,
                  num_classes=1000,
-                 include_top=True,
-                 groups=1,
-                 width_per_group=64):
+                 include_top=True):
         super(ResNet, self).__init__()
         self.include_top = include_top
         self.in_channel = 64
